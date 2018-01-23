d4f8abce847681133520123828fffe980a47bfe9
wenfeng_xi <wenfeng_xi@dnt.com.cn>
2018-01-23 17:07:59 +0800

    允许设置多个服务器

--- tasks/pubdnt.js
+++ tasks/pubdnt.js
@@ -26,7 +26,7 @@ module.exports = function (grunt) {
         var Client = require('ssh2').Client;
         //异步操作
         var done = this.async();
-
+        exec('rm -f dist.tar.gz', {cwd: "dist/"})
         console.log("compressing dist.tar.gz...")
         var cmdStr = "tar -zcvf dist.tar.gz *";
         exec(cmdStr, {cwd: "dist/"}, function (err, stdout, stderr) {
@@ -34,47 +34,52 @@ module.exports = function (grunt) {
                 console.log('error:' + stderr + err);
                 done(false);
             } else {
-                console.log("transfering to remote " + target_server + " server...")
-                client_scp.scp('dist/dist.tar.gz', {
-                    host: target_server,
-                    username: username,
-                    password: password,
-                    path: '/root/'
-                }, function (err, stdout) {
-                    if (err) {
-                        console.log('error:' + stderr);
-                        done(false);
-                    } else {
-                        var docker_id = "$(docker ps -a | grep " + docker_name + " | grep Up | awk '{print $1}')";
-                        console.log('ssh2 connecting...');
-                        var conn = new Client();
-                        conn.on('ready', function () {
-                            console.log("unpressing files...");
-                            conn.exec("docker cp dist.tar.gz $(docker ps -a | grep " + docker_name + " | grep Up | awk '{print $1}'):/code;  docker exec -i $(docker ps -a | grep " + docker_name + " | grep Up | awk '{print $1}') tar zxf /code/dist.tar.gz -C /code;", function (err, stream) {
-                                if (err) {
-                                    done(false);
-                                }
-                                stream.on('close', function (code, signal) {
-                                    console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-                                    done(true)
-                                    conn.end();
-                                }).on('data', function (data) {
-                                    console.log('STDOUT2: ' + data);
-                                }).stderr.on('data', function (data) {
-                                        console.log('STDERR: ' + data);
-                                        done(false);
-                                    });
-                            });
-                        }).connect({
-                            host: target_server,
-                            port: target_port,
-                            username: username,
-                            password: password
-                        });
-                    }
-                });
+                target_server.forEach(function(v){
+                    link_all_server(v)
+                })
             }
         });
-    });
 
+        function link_all_server(my_target_server){
+            console.log("transfering to remote " + my_target_server + " server...")
+            client_scp.scp('dist/dist.tar.gz', {
+                host: my_target_server,
+                username: username,
+                password: password,
+                path: '/root/'
+            }, function (err, stdout) {
+                if (err) {
+                    console.log('error:' + stderr);
+                    done(false);
+                } else {
+                    var docker_id = "$(docker ps -a | grep " + docker_name + " | grep Up | awk '{print $1}')";
+                    console.log('ssh2 connecting...');
+                    var conn = new Client();
+                    conn.on('ready', function () {
+                        console.log("unpressing files...");
+                        conn.exec("docker cp dist.tar.gz $(docker ps -a | grep " + docker_name + " | grep Up | awk '{print $1}'):/code;  docker exec -i $(docker ps -a | grep " + docker_name + " | grep Up | awk '{print $1}') tar zxf /code/dist.tar.gz -C /code;", function (err, stream) {
+                            if (err) {
+                                done(false);
+                            }
+                            stream.on('close', function (code, signal) {
+                                console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+                                // done(true)
+                                conn.end();
+                            }).on('data', function (data) {
+                                console.log('STDOUT2: ' + data);
+                            }).stderr.on('data', function (data) {
+                                console.log('STDERR: ' + data);
+                                done(false);
+                            });
+                        });
+                    }).connect({
+                        host: my_target_server,
+                        port: target_port,
+                        username: username,
+                        password: password
+                    });
+                }
+            });
+        }
+    });
 };
